d378691d75477fb062ce833e31ab99933c4b9fb6
Alba Gustems <albagustems@gmail.com>
2020-07-01 23:28:10 +0200

    Email v1. working. Pending refactoring?

--- configs/nodemailer.config.js
+++ configs/nodemailer.config.js
@@ -1,10 +1,14 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_ACCOUNT,
     pass: process.env.GMAIL_PASSWORD
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 })
 
--- controllers/pages.controllers.js
+++ controllers/pages.controllers.js
@@ -2,7 +2,7 @@ require('../configs/session.config');
 
 const mongoose = require('mongoose');
 const uploadCloud = require('../configs/cloudinary.config');
-const transporter = require('../configs/nodemailer.config')
+const transporter = require('../configs/nodemailer.config');
 const Animal = require('../models/Animal.model');
 const Adoption = require('../models/Adoption.model');
 const {
@@ -302,6 +302,33 @@ const adoptAnimal = async (req, res, next) => {
     }, {
       new: true
     })
+
+    const adopt = await Adoption.findById(adoption._id).populate('owner').populate('host');
+
+    contentHTML = `<div style="background: #4D716D; height: 100px; background-size: cover;
+    background-repeat: no-repeat; text-align: center; padding: 35px; font-size:1.3em; color:#cce0dd;">
+    <h1>Someone is interested in <strong> ${animal.name}, ${adopt.owner.fullname}!</strong></h1>
+  </div>
+  <section style="padding: 20px; margin: 50px; border: 2px solid #4D716D; border-radius:40px; text-align: center;">
+    <h2>ADOPTION PETITION</h2>
+    <p>The user ${adopt.host.username} in the <strong>Rehome Me</strong> webpage is interested in rehoming
+      ${animal.name} and being it's forever home!
+    </p>
+    <p>Please, log in at <a href="http://rehome-me.herokuapp.com/">Rehome Me</a> as soon as you can to accept or reject
+      this petition.</p>
+    <div>
+      <img src="https://d17fnq9dkz9hgj.cloudfront.net/uploads/2015/09/hfh-dog-hero.jpg" alt="dog-adoption">
+    </div>
+  </section>`
+
+    const info = await transporter.sendMail({
+      from: process.env.GMAIL_ACCOUNT,
+      to: adopt.owner.email,
+      subject: `Someone is interested ${animal.name}, ${adopt.owner.fullname}!`,
+      text: 'We are sad to see you go',
+      html: contentHTML
+    }, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
+
     res.redirect('/user-profile');
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
@@ -423,17 +450,37 @@ const getSitterDetails = async (req, res, next) => {
 
 const deleteUser = async (req, res, next) => {
   try {
-
     const userMail = await User.findById(req.params.userId)
 
-    const mailOptions = {
+    contentHTML = `<div style="background: #4D716D; height: 100px; background-size: cover;
+    background-repeat: no-repeat; text-align: center; padding: 35px; font-size:1.5em; color:#cce0dd;">
+    <h1>We are sorry to see yo go <strong>${userMail.fullname}</strong></h1>
+  </div>
+  <section
+    style="padding: 20px; margin: 50px; border: 2px solid #4D716D; border-radius:40px; text-align: center;">
+    <h2>ACCOUNT CANCELLATION</h2>
+    <p>This is a confirmation that your <strong>Rehome Me</strong> account has been cancelled.<br>
+      We are sorry to see you go, and if there is anything that you didn't like of our service, please let us now so
+      that
+      we can improve it.
+    </p>
+    <div>
+      <img
+        src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2019/07/15092404/do-dogs-grieve-other-dogs.jpg"
+        alt="sad-dog">
+    </div>
+    <p><b>If you want to use our services again, we'll be at <a href="http://rehome-me.herokuapp.com/">Rehome Me</a>
+        for
+        you!
+        :)</b</p> </section>`
+
+    const info = await transporter.sendMail({
       from: process.env.GMAIL_ACCOUNT,
       to: userMail.email,
-      subject: 'We are sorry to see you go :(',
-      text: 'We are sorry to see you go. If you ever need us again, we will be here for you! In the meantime, we deleted all your data, including your animals and personal information from our database.',
-    }
-
-    transporter.sendMail(mailOptions, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
+      subject: 'We are sorry to see you go ' + userMail.fullname,
+      text: 'We are sad to see you go',
+      html: contentHTML
+    }, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
 
     const user = await User.deleteOne({
       _id: req.params.userId
@@ -467,6 +514,35 @@ const hireSitter = async (req, res, next) => {
       status: 'pending'
     });
 
+    const sitt = await Adoption.findById(
+      sitting._id
+    ).populate('owner').populate('host');
+
+    contentHTML = `<div style="background: #4D716D; height: 100px; background-size: cover;
+    background-repeat: no-repeat; text-align: center; padding: 35px; font-size:1.5em; color:#cce0dd;">
+    <h1>Someone is interested in your sitting service <strong>${sitt.host.fullname}</strong></h1>
+  </div>
+  <section style="padding: 20px; margin: 50px; border: 2px solid #4D716D; border-radius:40px; text-align: center;">
+    <h2>SITTING PETITION</h2>
+    <p>The user ${sitt.owner.username} in the <strong>Rehome Me</strong> webpage is interested in your services as a
+      sitter!<br>
+      The pet's check-in date would be <strong>${sitt.checkin}</strong> and it will be returning to it's owner the
+      <strong>${sitt.checkout}</strong> as soon as you can to accept or reject this petition.
+    </p>
+    <p>Please, log in at <a href="http://rehome-me.herokuapp.com/">Rehome Me</a></p>
+    <div>
+      <img src="https://www.veterinarypracticenews.com/wp-content/uploads/2018/06/Pet-Sitter.jpg" alt="dog-sitter">
+    </div>
+  </section>`
+
+    const info = await transporter.sendMail({
+      from: process.env.GMAIL_ACCOUNT,
+      to: sitt.host.email,
+      subject: 'Someone is interested in your service ' + sitt.host.fullname,
+      text: 'We are sad to see you go',
+      html: contentHTML
+    }, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
+
     res.redirect('/user-profile');
 
   } catch (error) {
@@ -490,15 +566,48 @@ const acceptAdoption = async (req, res, next) => {
       status: 'accepted'
     }, {
       new: true
-    }).populate('animal');
+    }).populate('animal').populate('host').populate('owner');
 
     if (adoption.type === 'permanent') {
       const animalAdopted = await Animal.findByIdAndUpdate(adoption.animal._id, {
         adopted: 'adopted'
       }, {
         new: true
-      })
+      });
     }
+
+    contentHTML = `<div style="background: #4D716D; height: 100px; background-size: cover;
+    background-repeat: no-repeat; text-align: center; padding: 35px; font-size:1.5em; color:#cce0dd;">
+    <h1>The process has been closed!</h1>
+  </div>
+  <section style="padding: 20px; margin: 50px; border: 2px solid #4D716D; border-radius:40px; text-align: center;">
+    <h2>PROCESS CLOSED</h2>
+    <p>The process opened about the ${adoption.animal.name} in the <strong>Rehome Me</strong> webpage has been
+      closed.<br>
+      The actual petition status is ${adoption.status}.
+      <ul style="list-style-type: none">
+        <li><b>Animal's name</b>: ${adoption.animal.name}</li>
+        <li><b>Current owner</b>: ${adoption.owner.username}</li>
+        <li><b>Owner's email</b>: ${adoption.owner.email}</li>
+        <li><b>Host</b>: ${adoption.host.username}</li>
+        <li><b>Host's email</b>: ${adoption.host.email}</li>
+      </ul>
+    </p>
+    <p>To obtain more details, please log in at <a href="http://rehome-me.herokuapp.com/">Rehome Me</a>.</p>
+    <div>
+      <img
+        src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1195,w_2127,x_0,y_0/f_auto,q_auto,w_1100/v1586626259/shape/mentalfloss/606627-gettyimages-1080967246.jpg"
+        alt="process-finished">
+    </div>
+  </section>`
+
+    const info = await transporter.sendMail({
+      from: process.env.GMAIL_ACCOUNT,
+      to: `${adoption.host.email}, ${adoption.owner.email}`,
+      subject: 'Your opened process in Rehome Me has been closed!',
+      text: 'We are sad to see you go',
+      html: contentHTML
+    }, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
 
     res.redirect('/user-profile')
   } catch (error) {
@@ -512,13 +621,46 @@ const rejectAdoption = async (req, res, next) => {
       status: 'rejected'
     }, {
       new: true
-    }).populate('animal');
+    }).populate('animal').populate('owner').populate('host');
 
     const animalAdopted = await Animal.findByIdAndUpdate(adoption.animal._id, {
       adopted: 'not-adopted'
     }, {
       new: true
     })
+
+    contentHTML = `<div style="background: #4D716D; height: 100px; background-size: cover;
+    background-repeat: no-repeat; text-align: center; padding: 35px; font-size:1.5em; color:#cce0dd;">
+    <h1>The process has been closed!</h1>
+  </div>
+  <section style="padding: 20px; margin: 50px; border: 2px solid #4D716D; border-radius:40px; text-align: center;">
+    <h2>PROCESS CLOSED</h2>
+    <p>The process opened about the ${adoption.animal.name} in the <strong>Rehome Me</strong> webpage has been
+      closed.<br>
+      The actual petition status is ${adoption.status}.
+      <ul style="list-style-type: none">
+        <li><b>Animal's name</b>: ${adoption.animal.name}</li>
+        <li><b>Current owner</b>: ${adoption.owner.username}</li>
+        <li><b>Owner's email</b>: ${adoption.owner.email}</li>
+        <li><b>Host</b>: ${adoption.host.username}</li>
+        <li><b>Host's email</b>: ${adoption.host.email}</li>
+      </ul>
+    </p>
+    <p>To obtain more details, please log in at <a href="http://rehome-me.herokuapp.com/">Rehome Me</a>.</p>
+    <div>
+      <img
+        src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1195,w_2127,x_0,y_0/f_auto,q_auto,w_1100/v1586626259/shape/mentalfloss/606627-gettyimages-1080967246.jpg"
+        alt="process-finished">
+    </div>
+  </section>`
+
+    const info = await transporter.sendMail({
+      from: process.env.GMAIL_ACCOUNT,
+      to: `${adoption.host.email}, ${adoption.owner.email}`,
+      subject: 'Your opened process in Rehome Me has been closed!',
+      text: 'We are sad to see you go',
+      html: contentHTML
+    }, (error, info) => error ? console.log(error) : console.log('Email sent: ' + info.response))
 
     res.redirect('/user-profile')
   } catch (error) {
