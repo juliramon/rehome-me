afe56b88e61899e3b028a0c4f4ebab5fbb9efbea
Juli Ramon <juli@nexumlabs.com>
2020-07-02 00:32:01 +0200

    implemented tabs selector for the user profile

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+let tabs = document.querySelectorAll('.user-profile .fake-link');
+let activeTab = tabs[localStorage.getItem('activeTab')];
+activeTab.classList.add('active');
+
+tabs.forEach((el, index) => el.addEventListener('click', () => {
+  activeTab.classList.remove('active');
+  el.classList.add('active');
+  localStorage.setItem('activeTab', index);
+}));
